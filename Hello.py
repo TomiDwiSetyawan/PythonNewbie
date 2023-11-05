3a4effa18082fbfda697693af482b532f134a2ee
TomiDwiSetyawan <tomidwisetyawan1@gmail.com>
2023-11-05 09:44:05 +0700

    'percabangan\'

--- Hello.py
+++ Hello.py
@@ -2,3 +2,20 @@ print('Hello world')
 print(1+3)
 
 "Belajar Python"  #single lane
+
+print("Luas = ", 3*3, " cm")
+print("Luas = %2d " % (5*3), " cm")
+
+
+# nilai_sisi = int(input("Masukkan Nilai Sisi : "))
+# print("Luas = " , nilai_sisi**2)
+
+# nilai1 = int(input("Masukkan Nilai1 : "))
+# nilai2 = int(input("Masukkan Nilai2 : "))
+# nilai3 = int(input("Masukkan Nilai3 : "))
+# print("Total Luas = ", ((nilai1**2)+(nilai2**2)+(nilai3**2)))
+
+
+passs = input("Massukan Password : ")
+if(passs=="123"):
+    print("great")
